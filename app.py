83f851f60744b365e8d99fe46d0aa4d4d2ade0d3
filipecardosogoncalves-sketch <filipe.cardoso.goncalves@gmail.com>
2026-01-07 10:58:43 +0000

    Mudança-css e html

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def inicio():
     return render_template("index.html")
 
 def guardar_utilizador(username, email, password): 
-    # Abre o ficheiro em modo de acrescentar (a = append) 
     f = open(ficheiro, "a", encoding="utf-8") 
     f.write(username + ";" + email + ";" + password + "\n")
     f.close()
