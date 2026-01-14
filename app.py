454c391fef4c0cc1c87490349df871beef1ae403
giastra <giastra>
2026-01-14 21:03:14 +0000

    nova área do grafico, opção de selecionar o arquivo e a opção de selecionar arquivo do pc para baixar no servidor

--- app.py
+++ app.py
@@ -6,35 +6,21 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
 import csv
-
+from glob import glob
 app = Flask(__name__)
 
 ficheiro = "datasets/utilizadores.dat"
 
-@app.route("/area_pessoal",methods=["GET", "POST"])
-def area_pessoal():
-    global Data, header
-    fileName=confirmar()   
-    print('file name ',fileName) 
-    with open(fileName, newline='', encoding='utf-8') as csvfile:
-        Reade = csv.reader(csvfile, delimiter=',', quotechar='"')
-        global Data, header
-        header = next(Reade)  # Skip header
-        Data = list(Reade) 
-    
-    grafico = filmsByGenreChart(header, Data)
-    y=''
-    x=''
-    for a in header:
-        y+=f'<option value="{a}">{a}</option>'
-        x+=f'<option value="{a}">{a}</option>'
-    return render_template("area_pessoal.html", grafico=grafico,y=y,x=x)
 
+
+# registrar utilizador
 def guardar_utilizador(username, email, password): 
     f = open(ficheiro, "a", encoding="utf-8") 
     f.write(username + ";" + email + ";" + password + "\n")
     f.close()
 
+
+# login
 def verificar_login(username, password):
     # Se o ficheiro não existir, não há utilizadores
     if not os.path.exists(ficheiro):
@@ -54,7 +40,7 @@ def verificar_login(username, password):
     f.close()
     return False
 
-
+# pg de registro
 @app.route("/registar", methods=["POST"])
 def registar():
     username = request.form["username"]
@@ -65,7 +51,7 @@ def registar():
 
     return redirect("/area_pessoal")
 
-
+# pg de login
 @app.route("/login", methods=["POST"])
 def login():
     username = request.form["username"]
@@ -77,8 +63,35 @@ def login():
         return render_template("index.html", msg="Username ou password incorretos.")
     
 
-fileName='./datasets/imdb_top_250_movies.csv'
-def filmsByGenreChart(header, Data):
+
+# escolher o data set
+@app.route("/data", methods=["GET", "POST"])
+def confirmar():
+    global arcaive
+    print(arcaive , 'tipo de arquivo no data')
+    fileName=f'./datasets/{arcaive}.csv'
+    print(fileName)
+    y=''
+    x=''
+    with open(fileName, newline='', encoding='utf-8') as csvfile:
+        Reade = csv.reader(csvfile, delimiter=',', quotechar='"')
+        global Data, header
+        header = next(Reade)  # Skip header
+        Data = list(Reade) 
+        csvfile.close()
+    for a in header:
+        y+=f'<option value="{a}">{a}</option>'
+        x+=f'<option value="{a}">{a}</option>'
+    
+    grafico = GenGrafico(header, Data)
+    if grafico == '':
+        return render_template("grafico.html", y=y,x=x)
+    else:
+        return render_template("grafico.html", grafico=grafico,y=y,x=x)
+    
+
+# gerar grafico
+def GenGrafico(header, Data):
     items = []              
     Count = []              
     
@@ -139,36 +152,67 @@ def filmsByGenreChart(header, Data):
         return chart1Path
 
 
-def confirmar():
-    arcaive = request.form.get('arcaive')
-    if arcaive == 'fs':
-        fileName='./datasets/fantasy_series.csv'
-    if arcaive == 'IMDB250':
-        fileName='./datasets/imdb_top_250_movies.csv'
-    if arcaive == 'IMDBVG':
-        fileName='./datasets/imdb-videogames.csv'
-    else:
-        fileName='./datasets/imdb_top_250_movies.csv'
-    return fileName
-
+# Tela inicial
 @app.route("/",methods=["GET", "POST"])
 def inicio():
-    global Data, header
-    fileName=confirmar()   
-    print('file name ',fileName) 
-    with open(fileName, newline='', encoding='utf-8') as csvfile:
-        Reade = csv.reader(csvfile, delimiter=',', quotechar='"')
-        global Data, header
-        header = next(Reade)  # Skip header
-        Data = list(Reade) 
     
-    grafico = filmsByGenreChart(header, Data)
-    y=''
-    x=''
-    for a in header:
-        y+=f'<option value="{a}">{a}</option>'
-        x+=f'<option value="{a}">{a}</option>'
-    return render_template("index.html", grafico=grafico,y=y,x=x)
+    return render_template("index.html")
+
+
+# area pessoal
+arcaive=''
+
+@app.route("/area_pessoal",methods=["GET", "POST"])
+def area_pessoal():
+   escrita=''
+   datasetsList = sorted(glob(r'./datasets/*.csv'))
+
+   print(datasetsList)
+
+   for nome in datasetsList:
+     nome=nome.split('\\')
+     nome=nome[1].split('.')
+     escrita+=f'<option value="{nome[0]}">{nome[0]}</option>'
+
+   if request.method=='POST': 
+        print(request.method)
+        global arcaive
+        
+        arcaive = request.form.get('arcaive')
+        print('acquivo : ',arcaive)
+
+   return render_template("area_pessoal.html",escrita=escrita,arcaive=arcaive)
+
+# uploude de arquivos
+UPLOAD_FOLDER = 'datasets'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Extensões permitidas
+ALLOWED_EXTENSIONS = {'csv'}
+
+def allowed_file(filename):
+    """Verifica se o arquivo tem uma extensão permitida."""
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    voltar="<form action='/area_pessoal' method='get'><button>voltar</button></form>"
+    if 'file' not in request.files:
+        return f"Nenhum arquivo enviado{voltar}"
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return f"Nenhum arquivo selecionado{voltar}"
+
+    if file and allowed_file(file.filename):
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        return f"Arquivo '{file.filename}' salvo com sucesso em {UPLOAD_FOLDER}! {voltar}"
+    else:
+        return f"Tipo de arquivo não permitido{voltar}"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
